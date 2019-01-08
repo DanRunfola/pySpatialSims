bc7dee1acd12a66f3b6078020d15078e5427dff3
DanRunfola <dan@danrunfola.com>
2019-01-08 09:24:26 -0500

    3D display

--- spatialSim.py
+++ spatialSim.py
@@ -9,7 +9,7 @@ def buildFrame(Xlim0, Xlim1, Ylim0, Ylim1, step):
     frame = []
     for x in range(Xlim0, Xlim1, step):
         for y in range(Ylim0, Ylim1, step):
-            frame.append([x,y,1])
+            frame.append([x,y,random.random()])
     return(frame)
 
 
@@ -18,21 +18,31 @@ def initFrameValues(frame, type="random", options=None):
     if(type == "random"):
         for obs in frame:
             valFrame.append([obs[0], obs[1], random.random()])
+        return valFrame
 
     if(type=="kriging"):
         x,y,z = zip(*frame)
         #Randomly sample from the input frame a set number of seeds to use
         #For kriging.
         # Set as a percentage 
-        sampleSize = options[0] * len(x)
+        sampleSize = int(options[0] * len(x))
         
         #Create our kriging matrix:
-        #Below is broken, working on it now.
         kMat = random.sample(frame, k=sampleSize)
-        print(kMat)
+        sam_x, sam_y, sam_z = zip(*kMat)
+        x =  [float(i) for i in x]
+        y =  [float(i) for i in y]
+        z =  [float(i) for i in z]
 
 
-    return valFrame
+
+        #Krig:
+        ok_krig = OrdinaryKriging(sam_x,sam_y,sam_z, variogram_model="spherical", variogram_parameters={'sill':0.5, 'range':5.0, 'nugget':0},verbose=False, enable_plotting=False)
+        #Get results:
+        z, ss = ok_krig.execute('points', x, y)
+        return(zip(x,y,z))
+
+    return False
             
 
 def vizFrame(frame, type="window", fPath = None):
@@ -56,4 +66,4 @@ def sampleFrame(n, frame, pStrat="random"):
 
 area = buildFrame(0,10,0,10, step=1)
 area_z = initFrameValues(area, type="kriging", options=[0.05])
-#vizFrame(area_z, type="window")
+vizFrame(area_z, type="window")
